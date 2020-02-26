748a146531a501041b853218628fc58dde4c6cd8
hemonugi <hemonugi@gmail.com>
2020-02-26 22:43:32 +1100

    Добавлен вывод названий кораблей с апи

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="starship" v-for="starship in starships" :key="starship.url">
+      <h2>{{starship.name}}</h2>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      starships: []
+    };
+  },
+  async mounted() {
+    const res = await fetch("https://swapi.co/api/starships/");
+    const starships = await res.json();
+    this.starships = starships.results;
+  }
 }
 </script>
 
@@ -15,6 +29,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 40px auto;
+  width: 400px;
+}
+
+.starship {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 1rem;
 }
 </style>
