617d67bf38fb10ec88c53e1c92d7aedf9b2c0fd0
hemonugi <hemonugi@gmail.com>
2020-03-01 23:59:01 +1100

    Содержимое списков вынесено в компонент, рефакторинг code style

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,45 @@
 <template>
-  <div id="app">
-    <SearchForm />
-    <hr />
-    <div class="starships" v-for="starship in starshipsList" :key="starship.name">
-        <h2 v-on:click="starship.show = !starship.show">{{starship.name}}</h2>
-        <transition name="fade">
-          <div class="list" v-if="starship.show">
-            <div v-for="(value, name) in starship" :key="name">
-              <template>
-                  <b>{{ name }}: </b>{{ value }}
-              </template>
-            </div>
-          </div>
-        </transition>
-    </div>
-    <Pagination :previousLink="previousList" :nextLink="nextList"/>
-  </div>
+	<div id="app">
+		<SearchForm/>
+		<hr/>
+		<div class="starships" v-for="starship in starshipsList" :key="starship.name">
+			<ItemsList :starship="starship"/>
+		</div>
+		<Pagination :previousLink="previousList" :nextLink="nextList"/>
+	</div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex"
+import {mapGetters, mapActions} from "vuex"
 import SearchForm from "./components/SearchForm";
+import ItemsList from "./components/ItemsList";
 import Pagination from "./components/Pagination";
+
 export default {
-  name: 'App',
-  computed: mapGetters(["starshipsList", "previousList", "nextList"]),
-  methods: mapActions(["getAll"]),
-  components: { SearchForm, Pagination },
-  async mounted() {
-    this.getAll();
-  }
+    name: 'App',
+    computed: mapGetters(["starshipsList", "previousList", "nextList"]),
+    methods: mapActions(["getAll"]),
+    components: {ItemsList, SearchForm, Pagination},
+    async mounted() {
+        this.getAll();
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 40px auto;
-  width: 400px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin: 40px auto;
+	width: 400px;
 }
 
 .starships {
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  margin-bottom: 1rem;
-}
-.list {
-  text-align: left;
-  margin: 1rem;
+	border: 1px solid #ccc;
+	border-radius: 5px;
+	margin-bottom: 1rem;
 }
 </style>
---
+++ src/components/ItemsList.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<h2 v-on:click="starship.show = !starship.show">{{starship.name}}</h2>
+		<transition name="fade">
+			<div class="list" v-if="starship.show">
+				<div v-for="(value, name) in starship" :key="name">
+					<template>
+						<b>{{ name }}: </b>{{ value }}
+					</template>
+				</div>
+			</div>
+		</transition>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ["starship"]
+}
+</script>
+
+<style scoped>
+.list {
+	text-align: left;
+	margin: 1rem;
+}
+</style>
